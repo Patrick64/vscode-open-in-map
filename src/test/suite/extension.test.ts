cf7b6badb6d1fb1b25277c83a2f67a2fe56134ba
Patrick Woodcock <patrick@patrickw.tech>
2023-08-05 19:11:50 +0100

    Correct publisher

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -6,71 +6,73 @@ import { openMapInWebview, getMapHtml } from '../../openMapInWebview';
 import * as openMapInWebviewModule from '../../openMapInWebview';
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
-	
-	let createWebviewPanelStub: sinon.SinonStub;
-    let onDidDisposeStub: sinon.SinonStub;
-	let getMapHtmlStub: sinon.SinonStub;
-	let asWebviewUriStub: sinon.SinonStub;
-	let revealStub: sinon.SinonStub;
-	let postMessageStub: sinon.SinonStub;
+  vscode.window.showInformationMessage('Start all tests.');
 
-    setup(() => {
+  let createWebviewPanelStub: sinon.SinonStub;
+  let onDidDisposeStub: sinon.SinonStub;
+  let getMapHtmlStub: sinon.SinonStub;
+  let asWebviewUriStub: sinon.SinonStub;
+  let revealStub: sinon.SinonStub;
+  let postMessageStub: sinon.SinonStub;
+
+  setup(() => {
     // Stub the getMapHtml function to return a fixed string
-    getMapHtmlStub = sinon.stub(openMapInWebviewModule, 'getMapHtml').returns(Promise.resolve('<html></html>'));
+    getMapHtmlStub = sinon
+      .stub(openMapInWebviewModule, 'getMapHtml')
+      .returns(Promise.resolve('<html></html>'));
     onDidDisposeStub = sinon.stub();
     postMessageStub = sinon.stub();
     revealStub = sinon.stub();
-    asWebviewUriStub = sinon.stub().returns(vscode.Uri.parse('someUri')); 
-    createWebviewPanelStub = sinon.stub(vscode.window, 'createWebviewPanel').returns({
-		onDidDispose: onDidDisposeStub,
-		reveal: revealStub,
+    asWebviewUriStub = sinon.stub().returns(vscode.Uri.parse('someUri'));
+    createWebviewPanelStub = sinon
+      .stub(vscode.window, 'createWebviewPanel')
+      .returns({
+        onDidDispose: onDidDisposeStub,
+        reveal: revealStub,
         webview: {
-			postMessage: postMessageStub,
-			asWebviewUri: asWebviewUriStub,
+          postMessage: postMessageStub,
+          asWebviewUri: asWebviewUriStub,
         },
-    } as unknown as vscode.WebviewPanel);
-    });
+      } as unknown as vscode.WebviewPanel);
+  });
 
-    teardown(() => {
-        sinon.restore();
-	});
-	
+  teardown(() => {
+    sinon.restore();
+  });
 
-    test('openMapInWebview should throw an error if no valid latitude/longitude values are found', async () => {
-        const selectedText = 'invalid text';
-        const context = { subscriptions: [] } as unknown as vscode.ExtensionContext;
+  test('openMapInWebview should throw an error if no valid latitude/longitude values are found', async () => {
+    const selectedText = 'invalid text';
+    const context = { subscriptions: [] } as unknown as vscode.ExtensionContext;
 
-        try {
-            await openMapInWebview(selectedText, context);
-            assert.fail('Expected function to throw an error');
-        } catch (err: any) {
-            expect(err.message).to.exist;
+    try {
+      await openMapInWebview(selectedText, context);
+      assert.fail('Expected function to throw an error');
+    } catch (err: any) {
+      expect(err.message).to.exist;
+    }
+  });
+
+  test('should create a webview panel with the correct arguments', async () => {
+    const selectedText = '51.501476,-0.140634';
+    const context = { subscriptions: [] } as unknown as vscode.ExtensionContext;
+
+    await openMapInWebview(selectedText, context);
+
+    expect(
+      createWebviewPanelStub.calledWith(
+        'openInMap',
+        'Open in map',
+        vscode.ViewColumn.Beside,
+        {
+          enableScripts: true,
+          retainContextWhenHidden: true,
         }
-    });
+      )
+    ).to.be.true;
 
-	test('should create a webview panel with the correct arguments', async () => {
-		const selectedText = '51.501476,-0.140634';
-		const context = { subscriptions: [] } as unknown as vscode.ExtensionContext;
-	
-		
-		await openMapInWebview(selectedText, context);
-	
-		expect(createWebviewPanelStub.calledWith(
-			'openInMap',
-			'Open in map',
-			vscode.ViewColumn.Beside,
-			{
-				enableScripts: true,
-				retainContextWhenHidden: true
-			}
-		)).to.be.true;
+    assert(createWebviewPanelStub.calledOnce);
+    assert(onDidDisposeStub.calledOnce);
 
-		assert(createWebviewPanelStub.calledOnce);
-        assert(onDidDisposeStub.calledOnce);
-	
-		createWebviewPanelStub.restore();
-	});
-	
-
+    createWebviewPanelStub.restore();
+  });
 });
